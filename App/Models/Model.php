414a1df1b60ae195ad9a61ba416ff7d42f4f714a
HaizorWill <thelines6@gmail.com>
2025-06-29 21:52:27 +0300

    Revert Model.php

--- App/Models/Model.php
+++ App/Models/Model.php
@@ -2,23 +2,6 @@
 namespace App\Models;
 
 class Model {
-    private string $id;
-    private string $name;
 
-    public function create() {
-
-    }
-
-    public function read() {
-
-    }
-
-    public function update() {
-
-    }
-
-    public function delete() {
-
-    }
 }
 ?>
