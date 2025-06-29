9ac0adb1d0bb268f841a755844da2f512180cecd
HaizorWill <thelines6@gmail.com>
2025-06-29 21:11:50 +0300

    Create Model.php

---
+++ App/Models/Model.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Models;
+
+class Model {
+    private string $id;
+    private string $name;
+
+    public function create() {
+
+    }
+
+    public function read() {
+
+    }
+
+    public function update() {
+
+    }
+
+    public function delete() {
+
+    }
+}
+?>
