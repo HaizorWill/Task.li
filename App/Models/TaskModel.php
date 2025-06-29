03421f55c566ce4e1d62b75968161c31fb70aa84
HaizorWill <thelines6@gmail.com>
2025-06-29 21:12:02 +0300

    Create TaskModel.php

---
+++ App/Models/TaskModel.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Models;
+
+class TaskModel extends Model {
+    private string $id;
+    private string $name;
+    private string $description;
+    private string $status;
+    private $created_at;
+    
+    public function __construct() {
+
+    }
+
+    public function create() {
+
+    }
+
+    public function read() {
+
+    }
+
+    public function update() {
+
+    }
+
+    public function delete() {
+
+    }
+}
+
+?>
