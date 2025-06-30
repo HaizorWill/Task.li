c50d33bbeca3ff9de966b441cb01ce9b5aae82b5
HaizorWill <thelines6@gmail.com>
2025-06-30 21:57:19 +0300

    Update TaskModel.php

--- App/Models/TaskModel.php
+++ App/Models/TaskModel.php
@@ -1,6 +1,10 @@
 <?php
 namespace App\Models;
 
+use App\Database\Manager;
+use App\Models\Model;
+use MongoDB;
+
 class TaskModel extends Model {
     private string $id;
     private string $name;
@@ -9,23 +13,27 @@ class TaskModel extends Model {
     private $created_at;
     
     public function __construct() {
-
+        $this->dbManager = Manager::getInstance();
+        $this->dbCollection = 'tasks';
     }
 
     public function create() {
-
+        $cursor = $this->queryCollection()->insertOne(array('name' => 'name'));
+        return $cursor->getInsertedId();
     }
 
-    public function read() {
-
+    public function read($query = []) {
+        $cursor = $this->queryCollection()->find($query);
+        return $cursor;
     }
 
     public function update() {
 
     }
 
-    public function delete() {
-
+    public function delete($query) {
+        $result = $this->queryCollection()->deleteOne(['_id' => new MongoDB\BSON\ObjectId($query)]);
+        return $result->getDeletedCount();
     }
 }
 
