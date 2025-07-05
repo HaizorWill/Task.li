dfe7fdd362a82c2e24e720544b03e054f591f79a
HaizorWill <thelines6@gmail.com>
2025-07-05 10:22:26 +0300

    Update dbInsert.php

--- Tests/dbInsert.php
+++ Tests/dbInsert.php
@@ -3,13 +3,28 @@ require "../vendor/autoload.php";
 
 use App\Models\TaskModel;
 
+$data = ['name' => 'name', 'description' => 'description'];
 $taskModel = new TaskModel();
-$result = $taskModel->create();
+$result = $taskModel->fromArray($data);
+#validate
+$result = $taskModel->toArray($taskModel);
+
+$taskModel->create($result);
+
 $read = $taskModel->read();
-foreach($read as $entry) {
-    print_r($entry);
-};
-$delete = $taskModel->delete($result);
-print_r($delete);
+$result = $taskModel->fromArray($read);
+#validate
+$result = $taskModel->toArray($taskModel);
+
+var_dump($result);
+// $result = $taskModel->serializeModel($entry);
+
+// echo '<br>' . '<br>';
+// foreach($read as $entry) {
+//     $result = $taskModel->toArray($entry);
+//     var_dump($result);
+// };
+#$delete = $taskModel->delete($result);
+#print_r($delete);
 
 ?>
