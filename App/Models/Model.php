3ebbbe502725316bbddd67b9ea23d9a47f1177e9
HaizorWill <thelines6@gmail.com>
2025-07-05 10:21:02 +0300

    Add fromArray/toArray methods

--- App/Models/Model.php
+++ App/Models/Model.php
@@ -8,5 +8,25 @@ abstract class Model {
     protected function queryCollection() {
         return $this->dbManager::getDatabase()->selectCollection($this->dbCollection);
     }
+
+    public function fromArray($data) {
+        foreach ($data as $key => $value) {
+            if(property_exists($this, $key)) {
+                $this->{$key} = $value;
+            }
+        }
+    }
+
+    // public abstract function Validate();
+
+    public function toArray($data) {
+        $dataArray = [];
+        foreach ($data as $key => $value) {
+            if(property_exists($this, $key)) {
+                $dataArray[$key] = $value;
+            }
+        }
+        return $dataArray;
+    }
 }
 ?>
